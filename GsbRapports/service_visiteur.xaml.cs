6854e4a4e28db579586179e160a0c8c5a57a4d3e
vip181 <vincent.senut30@gmail.com>
2023-04-18 00:05:30 +0200

    first commit gsb

---
+++ GsbRapports/service_visiteur.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GsbRapports
+{
+    /// <summary>
+    /// Logique d'interaction pour service_visiteur.xaml
+    /// </summary>
+    public partial class service_visiteur : Window
+    {
+        public service_visiteur()
+        {
+            InitializeComponent();
+        }
+
+       
+
+      
+        private void modifierMenue(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RechargerMenue(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AjouterMenue(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void QuitterMenue(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
